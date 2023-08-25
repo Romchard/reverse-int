8970bd6feee8c6d1ed2c758f6a54e32059b746c7
Romchard <skqaeryes@gmail.com>
2023-08-25 13:42:36 +0500

    feat: solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  n = Math.abs(n);
+  if (n < 10) {
+      return n;
+  }
+  let a = n.toString();
+  console.log(a);
+  let b = Array.from(a);
+  console.log(b);
+  let c = b.reverse();
+  console.log(c);
+  let d = c.join("");
+  console.log(d);
+  return Number(d);
+};
